bc0c5afd539b3f1ccb4c052ddfc4d63b2a125876
nayukiminace@163.com <nayukiminace@163.com>
2026-03-04 16:06:00 +0800

    1.0.2 修复一处windows路径BUG，发版。

--- src/main/java/com/xuesinuo/muppet/api/VersionApi.java
+++ src/main/java/com/xuesinuo/muppet/api/VersionApi.java
@@ -26,7 +26,7 @@ public class VersionApi {
     }
 
     /** 当前版本 */
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     private void uiVersion() {
         webClient.get(443, "www.xuesinuo.com", "/muppet-print/version").ssl(true).send()
